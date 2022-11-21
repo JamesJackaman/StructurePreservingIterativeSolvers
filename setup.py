251e388ae311951a550a6f3bd93e6a8245baf675
James Jackaman <james.jackaman@ntnu.no>
2022-11-21 17:13:29 +0100

    Add setup script to initialise plots subfolder and install additional dependencies to the venv

---
+++ setup.py
@@ -0,0 +1,27 @@
+'''
+Installs additional required packages and creates
+required subdirectories
+'''
+import os
+
+#Create directories
+if os.path.isdir("plots")==False:
+    os.system("mkdir plots")
+
+#Install extra modules (only inside Firedrake venv)
+try:
+    import firedrake
+except:
+    raise AssertionError('Users must run this script within a Firedrake virtual environment')
+try:
+    import scipy
+except:
+    os.system('pip3 install scipy')
+try:
+    import pandas
+except:
+    os.system('pip3 install pandas')
+try:
+    import pickle
+except:
+    os.system('pip3 install pickle')
